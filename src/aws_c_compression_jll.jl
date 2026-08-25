3297212dd4646e036b95e20d1e34af9983591923
jlbuild <juliabuildbot@gmail.com>
2026-08-25 18:42:26 +0000

    aws_c_compression_jll build 0.3.3+0

--- src/aws_c_compression_jll.jl
+++ src/aws_c_compression_jll.jl
@@ -5,5 +5,5 @@ using Base: UUID
 import JLLWrappers
 
 JLLWrappers.@generate_main_file_header("aws_c_compression")
-JLLWrappers.@generate_main_file("aws_c_compression", UUID("73a04cd5-f3d7-5bac-9290-e8adb709f224"))
+JLLWrappers.@generate_main_file("aws_c_compression", Base.UUID("73a04cd5-f3d7-5bac-9290-e8adb709f224"))
 end  # module aws_c_compression_jll
